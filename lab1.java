4f49925509004ccc27fdf03bd925efa61243d793
SonamChokiii <71283481+SonamChokiii@users.noreply.github.com>
2020-09-18 20:30:52 +0600

    Create lab1.java

---
+++ lab1.java
@@ -0,0 +1,13 @@
+public class lab1{
+	public static void main (String[] args){
+		int[] array;
+		array= new int[3];
+		array[0]= 1;
+		array[1]= 2;
+		array[2]=3;
+		for (int i=2; i>=0; i--) {
+		System.out.println(array[i]);
+
+		}
+	}
+}
